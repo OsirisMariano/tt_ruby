0088044d8ddec80eb76c4e8a7e2e30005d2e71a4
Osiris Mariano <oslimacontato@gmail.com>
2023-08-16 06:26:50 -0300

    feat: tt2

--- tt1/tt2.rb
+++ tt1/tt2.rb
@@ -4,3 +4,64 @@ formatado de acordo com a máscara 999.999.999-99. A função deve usar regex pa
 a validação da máscara e deve retornar verdadeiro ou falso.
 cpf = “461.636.517-23”
 =end
+
+def formatar_cpf(cpf)
+  cpf_formatado = cpf.gsub(/(\d{3})(\d{3})(\d{3})(\d{2})/, '\1.\2.\3-\4')
+  return cpf_formatado
+end
+
+def validar_cpf(cpf)
+  regex_numeros = /^\d{11}$/
+
+  if cpf.match?(regex_numeros)
+    return true
+  else
+    return false
+  end
+end
+
+def exibir_opcoes
+  puts "Opções:"
+  puts "1 - Tentar novamente"
+  puts "2 - Sair do programa"
+end
+
+loop do
+  puts "Digite os 11 números do CPF:"
+  cpf_numeros = gets.chomp
+
+  if validar_cpf(cpf_numeros)
+    cpf_formatado = formatar_cpf(cpf_numeros)
+    puts "CPF formatado: #{cpf_formatado}"
+
+    exibir_opcoes
+    escolha = gets.chomp.to_i
+
+    case escolha
+    when 1
+      next
+    when 2
+      puts "Saindo do programa. Até logo!"
+      break
+    else
+      puts "Opção inválida! Saindo do programa."
+      break
+    end
+  else
+    puts "CPF inválido! Digite apenas os 11 números."
+    exibir_opcoes
+    escolha = gets.chomp.to_i
+
+    case escolha
+    when 1
+      next
+    when 2
+      puts "Saindo do programa. Até logo!"
+      break
+    else
+      puts "Opção inválida! Saindo do programa."
+      break
+    end
+  end
+end
+
