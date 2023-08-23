73e02ef154e323aaf1696fd90f8db7431ec38a55
Osiris Mariano <oslimacontato@gmail.com>
2023-08-23 13:15:18 -0300

    tt9.rb

--- tt1/tt9.rb
+++ tt1/tt9.rb
@@ -1,3 +1,27 @@
 =begin
 Quais as vantagens e desvantagens do ruby ser uma linguagem dinamicamente tipada?
+
+VANTAGENS:
+1 - Produtividade: Ruby permite que os desenvolvedores sejam mais produtivos durante o desenvolvimento,
+alterando tipos de variáveis e estruturas de dados rapidamente
+
+2 - Código Conciso: A não exigência de declarações explícitas leva a um código mais conciso e legível.
+
+3 - Prototipagel rápida: A falta de restrições do tipo rígidas é benéfica durante as fases
+iniciais.
+
+4 - Mudanças Dinâmicas: Permite que você adapte seu código facilmente, evitando a necessidade de compilar
+ou redefinir tipos.
+
+DESVANTAGENS:
+
+1 - Erros em tempo de execução: Os erros podem acontecer durante a execução do progama, o que pode levar
+a bugs difíceis de identificar e depurar.
+
+2 - Falta de verificação estática: A ausência de verificação de tipos em tempo de compilação pode
+levar a erros que só são descobertos durante a execução.
+
+3 - Performace: O tempo de execução pode ter um impacto negativo na performace do programa
+
+4 - Refatoração complexa: Refatorar códgios me Ruby pode ser mais complexo, especialemente projetos grandes
 =end
